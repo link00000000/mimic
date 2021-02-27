352d32cc33c37c1abb937ec60ad6d83871794407
Logan Crandall <crandall.logan@gmail.com>
2021-02-27 13:17:59 -0500

    Move routes into Server constructor

--- Server.py
+++ Server.py
@@ -1,11 +1,18 @@
-from EventEmitter import EventEmitter
 from aiohttp import web
 from aiohttp.web_request import Request
 import asyncio
 from threading import Event
+from Pipes import StringMessage, Pipeable
 
 
-class Server(EventEmitter):
+class Server(Pipeable):
+    """
+    Async HTTP server, messages can be recieved by polling `pipe`.
+
+    @NOTE: Because we need access to `self` inside the route, all routes
+    must be defined inside of `__init__` instead of individual methods.
+    """
+
     host: str
     port: int
 
@@ -23,25 +30,31 @@ class Server(EventEmitter):
 
         self.stop_event = stop_event
 
-    @routes.get('/')
-    async def hello(request: Request):
-        return web.Response(text="Hello, world")
+        @self.routes.get('/')
+        async def hello(request: Request):
+            self._pipe.send(StringMessage("GET /"))
+            return web.Response(text="Hello, world")
 
     async def start(self):
-        self.emit('start')
+        """
+        Start listening to HTTP traffic
+        """
+        self._pipe.send(StringMessage("Web server starting..."))
 
         self.app.add_routes(self.routes)
 
         self.runner = web.AppRunner(self.app)
         await self.runner.setup()
 
-        self.site = web.TCPSite(self.runner, 'localhost', 8080)
+        self.site = web.TCPSite(self.runner, self.host, self.port)
         await self.site.start()
 
         print(f"aiohttp listening on http://{self.host}:{self.port}")
 
-        # Loop infinitely in 1 hour intervals
+        # Loop infinitely in 1 second intervals
         while True:
+            # If the `stop_event` has been set, cleanup and close the HTTP
+            # server
             if(self.stop_event != None and self.stop_event.is_set()):
                 await self.runner.cleanup()
                 return
