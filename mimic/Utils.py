bbb1dfc4863b282cce1188deafa387bf1ba394ce
Mason Roberts <mcr66@uakron.edu>
2021-03-09 19:31:55 -0500

    Move renaming function to its own file

--- mimic/Utils.py
+++ mimic/Utils.py
@@ -1,24 +1,6 @@
 """Miscellaneous utility functions."""
 import socket
-import winreg
 
-REG_PATH = r"CLSID\{860BB310-5D01-11d0-BD3B-00A0C911CE86}\Instance\{27B05C2D-93DC-474A-A5DA-9BBA34CB2A9C}"
-def change_camera(name: str = 'Mimic'):
-    """
-    Change the name associated with the camera by accessing its registry.
-
-    There is the possibility of permission issues if the the user is not
-    have full control of the camera registry entry
-
-    Params:
-        str: the desired name of the camera (defaults to Mimic)
-    """
-    try:
-        reg = winreg.ConnectRegistry(None, winreg.HKEY_CLASSES_ROOT)
-        obs_reg = winreg.OpenKey(reg, REG_PATH, 0, winreg.KEY_SET_VALUE)
-        winreg.SetValueEx(obs_reg, "FriendlyName", 0, winreg.REG_SZ, name)
-    except:
-        print("Could not change the camera's name!")
 
 def resolve_host() -> str:
     """
