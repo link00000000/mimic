6a643e371b7f27f485651e39fec98bb040139e98
Logan Crandall <crandall.logan@gmail.com>
2021-03-19 13:52:26 -0400

    Close all connections with GET /close

--- mimic/WebServer.py
+++ mimic/WebServer.py
@@ -1,4 +1,5 @@
 import asyncio
+import io
 import json
 import logging
 import os
@@ -9,7 +10,9 @@ from threading import Event
 import pyvirtualcam
 from aiohttp import web
 from aiohttp.web_request import Request
+from aiohttp.web_response import StreamResponse
 from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.mediastreams import MediaStreamError
 from aiortc.rtcpeerconnection import RemoteStreamTrack
 from av import VideoFrame
 
@@ -18,12 +21,10 @@ from mimic.Utils.Host import resolve_host
 
 ROOT = "mimic/public"
 
-logger = logging.getLogger("pc")
-pcs = set()
 
-
-async def start_web_server(stop_event: Event, pipe: Connection):
+async def start_web_server(stop_event: Event, pipe: Connection) -> None:
     cam = pyvirtualcam.Camera(640, 480, 30)
+    pcs: set[RTCPeerConnection] = set()
 
     async def show_frame(track: RemoteStreamTrack) -> VideoFrame:
         frame = await track.recv()
@@ -41,24 +42,37 @@ async def start_web_server(stop_event: Event, pipe: Connection):
     def log(message: str, level: int = logging.INFO):
         pipe.send(LogMessage(message, level))
 
+    async def close_all_connections() -> int:
+        for pc in pcs:
+            await pc.close()
+
+        num_pcs = len(pcs)
+        pcs.clear()
+
+        return num_pcs
+
     @web.middleware
-    async def logging_middleware(request: Request, handler):
+    async def logging_middleware(request: Request, handler) -> StreamResponse:
         log(f"{request.method} {request.path} - {request.remote}", logging.DEBUG)
         return await handler(request)
 
-    async def index(request):
+    async def index(request: Request) -> StreamResponse:
         content = open(os.path.join(ROOT, "index.html"), "r").read()
         return web.Response(content_type="text/html", text=content)
 
-    async def javascript(request):
+    async def javascript(request: Request) -> StreamResponse:
         content = open(os.path.join(ROOT, "app.js"), "r").read()
         return web.Response(content_type="application/javascript", text=content)
 
-    async def css(request):
+    async def css(request: Request) -> StreamResponse:
         content = open(os.path.join(ROOT, "app.css"), "r").read()
         return web.Response(content_type="text/css", text=content)
 
-    async def offer(request):
+    async def close(request: Request) -> StreamResponse:
+        num_connections = await close_all_connections()
+        return web.Response(text=f"Closed {num_connections} connection(s)")
+
+    async def offer(request: Request) -> StreamResponse:
         params = await request.json()
         offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
@@ -82,22 +96,26 @@ async def start_web_server(stop_event: Event, pipe: Connection):
                 pcs.discard(pc)
 
         @pc.on("track")
-        async def on_track(track):
+        async def on_track(track: RemoteStreamTrack):
             log(f"Track {track.kind} received")
 
             if track.kind != "video":
                 track.stop()
                 return
 
+            @track.on("ended")
+            async def on_ended():
+                log(f"Track {track.kind} ended")
+
             while True:
                 if track.readyState != "live":
                     break
 
-                await show_frame(track)
-
-            @track.on("ended")
-            async def on_ended():
-                log(f"Track {track.kind} ended")
+                try:
+                    await show_frame(track)
+                except MediaStreamError as error:
+                    if track.readyState == 'live':
+                        raise error
 
         # handle offer
         await pc.setRemoteDescription(offer)
@@ -113,12 +131,6 @@ async def start_web_server(stop_event: Event, pipe: Connection):
             ),
         )
 
-    async def shutdown():
-        # close peer connections
-        coros = [pc.close() for pc in pcs]
-        await asyncio.gather(*coros)
-        pcs.clear()
-
     ssl_context = ssl.SSLContext()
     ssl_context.load_cert_chain(
         "certs/selfsigned.cert", "certs/selfsigned.pem")
@@ -128,6 +140,7 @@ async def start_web_server(stop_event: Event, pipe: Connection):
     app.router.add_get("/app.js", javascript)
     app.router.add_get("/app.css", css)
     app.router.add_post("/offer", offer)
+    app.router.add_get('/close', close)
 
     runner = web.AppRunner(app, handle_signals=True)
     await runner.setup()
@@ -139,11 +152,11 @@ async def start_web_server(stop_event: Event, pipe: Connection):
     log(f"Server listening at https://{resolve_host()}:8080")
 
     while stop_event is None or not stop_event.is_set():
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.05)
 
     cam.close()
 
-    await shutdown()
+    await close_all_connections()
     await site.stop()
     await runner.shutdown()
     await runner.cleanup()
